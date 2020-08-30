71ad4872abe2f03ab88ee4e611b53654ac360812
ja-me-ch <60724072+ja-me-ch@users.noreply.github.com>
2020-08-30 15:05:12 -0400

    update

---
+++ Assignment_3_EFCore/Assignment_3_EFCore/MainWindow.xaml.cs
@@ -0,0 +1,248 @@
+﻿using BaseballLibrary;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment_3_EFCore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        BaseballLibrary.Player player = new Player();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }//end of MainWindow
+
+        public static string GetConnectionString()
+        {
+            string connectionString;
+
+            //Insert Baseball.mdf directory after AttachDbFile=
+            connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename= [insert Baseball.mdf directory here] ;Initial Catalog=Assignment_3_BaseballDB;Integrated Security=True;Connect Timeout=30";
+
+            return connectionString;
+        }//end of GetConnectionString
+
+        public List<BaseballLibrary.Player> SearchPlayerByLastName(string parameter)
+        {
+            List<BaseballLibrary.Player> players = new List<BaseballLibrary.Player>();
+
+            using (SqlConnection connection = new SqlConnection(GetConnectionString()))
+            {
+
+                string sqlCommandString = @"SELECT [PlayerID], [FirstName], [LastName], [BattingAverage] FROM [Players] WHERE [LastName] = @lastname";
+                SqlCommand sqlCommand = new SqlCommand(sqlCommandString, connection);
+
+                sqlCommand.Parameters.AddWithValue("@lastname", parameter);
+
+                connection.Open();
+
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    players.Add(new BaseballLibrary.Player
+                    {
+                        PlayerID = reader.GetInt32(0),
+                        FirstName = reader.GetString(1),
+                        LastName = reader.GetString(2),
+                        BattingAverage = reader.GetDecimal(3)
+                });
+                }//end while
+
+                connection.Close();
+
+            }//end of using
+
+            return players;
+
+        }//end of SearchPlayerByLastName
+
+        private void btn_Add_Player_Click(object sender, RoutedEventArgs e)
+        {
+            bool textbox_check_pass = false;
+            while (textbox_check_pass == false)
+            {
+
+                if (String.IsNullOrEmpty(txbx_FirstName.Text) == true)
+                {
+                    textbox_check_pass = false;
+                    break;
+                }
+                else
+                {
+                    player.FirstName = txbx_FirstName.Text;
+                }
+
+                if (String.IsNullOrEmpty(txbx_LastName.Text) == true)
+                {
+                    textbox_check_pass = false;
+                    break;
+                }
+                else
+                {
+                    player.LastName = txbx_LastName.Text;
+                }
+
+                //if (String.IsNullOrEmpty(txbx_PlayerID.Text) == true)
+                //{
+                //    textbox_check_pass = false;
+                //    break;
+                //}
+                //else
+                //{
+                //    int result;
+                //    if (int.TryParse(txbx_PlayerID.Text, out result) == false)
+                //    {
+                //        textbox_check_pass = false;
+                //        break;
+                //    }
+                //    Trace.WriteLine(result);
+                //    player.PlayerID = result;
+                //}
+
+                if (String.IsNullOrEmpty(txbx_BattingAverage.Text) == true)
+                {
+                    textbox_check_pass = false;
+                    Trace.WriteLine("ERROR: Batting Average is NULL or empty.");
+                    break;
+                }
+                else
+                {
+                    decimal result;
+                    if (decimal.TryParse(txbx_BattingAverage.Text, out result) == false)
+                    {
+                        textbox_check_pass = false;
+                        Trace.WriteLine("ERROR: Batting Average is not convertable to decimal.");
+                        break;
+                    }
+                    if (CheckScaleAndPrecision(result) == false)
+                    {
+                        textbox_check_pass = false;
+                        break;
+                    }
+                    else
+                    {
+                        Trace.WriteLine(result);
+                        player.BattingAverage = result;
+                    }
+                    
+                }
+
+                textbox_check_pass = true;
+                
+            }//end of while
+
+            if (textbox_check_pass == true) //if all the checks are passed = true
+            {
+                using (var connection = new SqlConnection(GetConnectionString()))
+                {
+
+                    string sqlQuery = "INSERT INTO [Players] ([FirstName], [LastName], [BattingAverage]) VALUES (@fname, @lname, @bataverage)";
+                    SqlCommand sqlCommand = new SqlCommand(sqlQuery, connection);
+
+                    //SqlParameter playerID_parameter = new SqlParameter("@playerID", System.Data.SqlDbType.Int);
+                    //playerID_parameter.Value = player.PlayerID;
+
+                    sqlCommand.Parameters.AddWithValue("@fname", player.FirstName);
+                    sqlCommand.Parameters.AddWithValue("@lname", player.LastName);
+
+                    SqlParameter battingAverage_parameter = new SqlParameter("@bataverage", System.Data.SqlDbType.Decimal);
+                    battingAverage_parameter.Value = ((decimal)player.BattingAverage);
+                    battingAverage_parameter.Precision = 3;
+                    battingAverage_parameter.Scale = 3;
+
+                    //sqlCommand.Parameters.Add(playerID_parameter);
+                    sqlCommand.Parameters.Add(battingAverage_parameter);
+
+                    connection.Open();
+
+                    sqlCommand.ExecuteNonQuery();
+
+                    connection.Close();
+                }//end of using
+            }//end of if
+            else //if checks fail
+            {
+
+            }
+
+        }//end of btn_Add_Player_Click
+
+        public bool CheckScaleAndPrecision(decimal number)
+        {
+            int precision = 3;
+            int scale = 3;
+
+            SqlDecimal sqlDecimal = new SqlDecimal(number);
+            if (sqlDecimal.Precision > precision || sqlDecimal.Scale > scale)
+            {
+                Trace.WriteLine("Invalid precision and scale.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }//end of ConvertScaleAndPrecision
+
+        private void btn_Search_Click(object sender, RoutedEventArgs e)
+        {
+            datagrid_Baseball.ItemsSource = SearchPlayerByLastName(txbx_LastName.Text);
+        }//end of btn_Search_Click
+
+        private void btn_ViewAllPlayers_Click(object sender, RoutedEventArgs e)
+        {
+            List<BaseballLibrary.Player> players = new List<BaseballLibrary.Player>();
+
+            using (var connection = new SqlConnection(GetConnectionString()))
+            {
+
+                string sqlQuery = "SELECT [PlayerID], [FirstName], [LastName], [BattingAverage] FROM [Players]";
+                SqlCommand sqlCommand = new SqlCommand(sqlQuery, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+
+                while (reader.Read())
+                {
+                    players.Add(new BaseballLibrary.Player
+                    {
+                        PlayerID = reader.GetInt32(0),
+                        FirstName = reader.GetString(1).ToString(),
+                        LastName = reader.GetString(2).ToString(),
+                        BattingAverage = reader.GetDecimal(3)
+                    });
+                }//end of while
+
+                datagrid_Baseball.ItemsSource = players;
+
+                connection.Close();
+
+            }//end of using
+
+        }//end of btn_ViewAllPlayers_Click
+    }//end of MainWindow : Window partial class
+
+}//end of namespace
---
+++ Assignment_3_LINQ/Assignment_3_LINQ/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/*
+Q1.	List the names of the countries in alphabetical order
+Q2.	List the names of the countries in descending order of number of resources
+Q3.	List the names of the countries that shares a border with Argentina
+Q4.	List the names of the countries that has more than 10,000,000 population
+Q5.	List the country with highest population
+Q6.	List all the religion in south America in dictionary order
+ */
+
+namespace Assignment_3_LINQ
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txbl_TextArea.Text = "Sample Text";
+        }
+
+        public void SetText(string text)
+        {
+            txbl_TextArea.Text = "";
+            txbl_TextArea.Text = text;
+        }
+
+        private void Q1_Click(object sender, RoutedEventArgs e)
+        {
+
+            var alphabeticalList = from items in Country.GetCountries()
+                                   orderby items.Name
+                                   select items.Name;
+
+            string text = "";
+            foreach (var item in alphabeticalList)
+            {
+                text = text + item + "\n";
+            }
+            SetText(text);
+        }//end of Q1_Click
+
+        private void Q2_Click(object sender, RoutedEventArgs e)
+        {
+            var countries_descending_order_by_resources = from items in Country.GetCountries()
+                                                          orderby items.Resources.Count descending
+                                                          select new
+                                                          {
+                                                              Country = items.Name,
+                                                              ResourceCount = items.Resources.Count
+                                                          };
+
+            string text = "";
+            foreach(var item in countries_descending_order_by_resources)
+            {
+                text = $"{text}{item.Country} : {item.ResourceCount}\n";
+                //text = text + item + "\n";
+            }
+
+            SetText(text);
+        }
+
+        private void Q3_Click(object sender, RoutedEventArgs e)
+        {
+            var countries_share_border_argentina = from items in Country.GetCountries()
+                                                   where items.Borders.Contains("Argentina")
+                                                   orderby items.Name
+                                                   select new
+                                                   {
+                                                       Country = items.Name,
+                                                   };
+
+            string text = "";
+            foreach (var item in countries_share_border_argentina)
+            {
+
+                text = $"{text}{item.Country}\n";
+            }
+
+            SetText(text);
+
+        }
+
+        private void Q4_Click(object sender, RoutedEventArgs e)
+        {
+            var countries_with_pop_over_10mil = from item in Country.GetCountries()
+                                                where item.Population > 10000000
+                                                orderby item.Population descending
+                                                select new
+                                                {
+                                                    Country = item.Name,
+                                                    Population = item.Population
+                                                };
+
+            string text = "";
+            foreach (var item in countries_with_pop_over_10mil)
+            {
+
+                text = $"{text}{item.Country} : {item.Population}\n";
+            }
+
+            SetText(text);
+
+        }
+
+        private void Q5_Click(object sender, RoutedEventArgs e)
+        {
+            var country_highest_population = (from item in Country.GetCountries()
+                                              orderby item.Population descending
+                                              select new
+                                              {
+                                                  Country = item.Name,
+                                                  Population = item.Population
+                                              }).Take(1);
+
+            string text = "";
+
+            foreach (var item in country_highest_population)
+            {
+                text += $"{item.Country} : {item.Population}";
+            }
+
+            SetText(text);
+        }//end of Q5_Click
+
+        private void Q6_Click(object sender, RoutedEventArgs e)
+        {
+
+            var all_religion_in_sa_ascending = (from country in Country.GetCountries()
+                                                from religion in country.Religions
+                                                orderby religion
+                                                select religion).Distinct();
+
+            string text = "";
+
+            foreach (var religion in all_religion_in_sa_ascending)
+            {
+                text = $"{text}{religion}\n";
+            }
+
+            SetText(text);
+
+
+        }//end of Q6_Click
+    }//end of partial class MainWindow : Window
+}//end of namespace
